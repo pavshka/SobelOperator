774a79f159babc5e652136f76e9ed0215e88c48a
Paul Shumeika <knux.wild@gmail.com>
2014-05-04 23:32:04 +0300

    Small fixes.

--- src/sobel.cpp
+++ src/sobel.cpp
@@ -1,12 +1,3 @@
-/* *
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include "bitmap_image.hpp"
@@ -23,10 +14,15 @@ struct rgb sobel_operator(int x, int y, bitmap_image image)
   pixel = new struct rgb;
   sobel_matrix = new int[3][3];
 
-  // image borders check
-  if (x == 0)
-	for (int i=0; i<3; i++)
-;
+  // sobel matrix filling with image borders check
+  image.get_pixel()
+
+  for (int i=0; i<3; i++)
+  {
+    if (y == 0)
+      for (int j=0; j<3; j++)
+    	sobel_atrix[i][]
+  }
 
 
   return pixel;
@@ -34,21 +30,21 @@ struct rgb sobel_operator(int x, int y, bitmap_image image)
 
 int main(void)
 {
-	pixel = new struct rgb;
-    bitmap_image image("lena.bmp");
-    if (!image)
-	  {
-	    printf("Error - Failed to open lena.bmp \n");
-	    return 1;
-	  }
+  pixel = new struct rgb;
+  bitmap_image image("lena.bmp");
+  if (!image)
+    {
+      printf("Error - Failed to open lena.bmp \n");
+      return 1;
+    }
 
-	for (int y=0; y < image.width(); y++)
-      for (x=0; x < image.height(); x++)
-	    {
-    	  pixel = sobel_operator(x, y, image);
-	      image.set_pixel(x, y, pixel.red, pixel.green, pixel.blue);
-		}
+  for (int y=0; y < image.width(); y++)
+    for (x=0; x < image.height(); x++)
+      {
+        pixel = sobel_operator(x, y, image);
+        image.set_pixel(x, y, pixel.red, pixel.green, pixel.blue);
+      }
 
-	image.save_image("sobelized.bmp");
-	return 0;
+  image.save_image("sobelized.bmp");
+  return 0;
 }
